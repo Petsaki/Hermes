af8c06d5c3f8f831e38ee1ad0b846ffefdc853de
Petsaki <Petsaki@users.noreply.github.com>
2021-03-16 13:19:51 +0200

    Login prototype v2

--- app/src/main/java/com/petsaki/epaketo/MainActivity.java
+++ app/src/main/java/com/petsaki/epaketo/MainActivity.java
@@ -2,13 +2,22 @@ package com.petsaki.epaketo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_Epaketo);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+        Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show();
     }
 }
