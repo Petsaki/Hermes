02baefcc808ae47b6549fcfe55234aeb4fb159d8
Petsaki <Petsaki@users.noreply.github.com>
2021-04-17 23:15:03 +0300

    beta ver. 1.31

--- app/src/main/java/com/petsaki/epaketo/HelperAdapter.java
+++ app/src/main/java/com/petsaki/epaketo/HelperAdapter.java
@@ -122,19 +122,19 @@ public class HelperAdapter extends RecyclerView.Adapter<HelperAdapter.NewViewHol
 
             map.getUiSettings().setAllGesturesEnabled(false);
             map.getUiSettings().setMapToolbarEnabled(false);
-//            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
-//                    != PackageManager.PERMISSION_GRANTED
-//                    && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
-//                    != PackageManager.PERMISSION_GRANTED) {
-//                // TODO: Consider calling
-//                //    ActivityCompat#requestPermissions
-//                // here to request the missing permissions, and then overriding
-//                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//                //                                          int[] grantResults)
-//                // to handle the case where the user grants the permission. See the documentation
-//                // for ActivityCompat#requestPermissions for more details.
-//                return;
-//            }
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
 //            BitmapDrawable bitmapdraw = (BitmapDrawable)context.getResources().getDrawable(R.drawable.icons8_package_48);
 //            Bitmap b = bitmapdraw.getBitmap();
 //            Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
